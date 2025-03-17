c23443e4af5bded425652e1c874c94afc220fd5f
Hüseyin Çiçek <mrcicek.huseyin@gmail.com>
2025-03-17 20:38:08 +0300

    auto mapper ile mappleme işlemi yapıldı.

--- src/projects/HospitalAppointmentProject.API/Program.cs
+++ src/projects/HospitalAppointmentProject.API/Program.cs
@@ -2,6 +2,7 @@ using HospitalAppointmentProject.DataAccess.Contexts;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,6 +13,8 @@ builder.Services.AddDbContext<BaseDbContext>(opt =>
 {
     opt.UseSqlServer(builder.Configuration.GetConnectionString("SqlConnection"));
 });
+builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
---
+++ src/projects/HospitalAppointmentProject.Service/Mappers/Profiles/AutoMapperConfig.cs
@@ -0,0 +1,47 @@
+﻿using AutoMapper;
+using HospitalAppointmentProject.Model.Dtos.Appointments;
+using HospitalAppointmentProject.Model.Dtos.Doctors;
+using HospitalAppointmentProject.Model.Dtos.Hospitals;
+using HospitalAppointmentProject.Model.Dtos.Patients;
+using HospitalAppointmentProject.Model.Dtos.Roles;
+using HospitalAppointmentProject.Model.Dtos.UserRoles;
+using HospitalAppointmentProject.Model.Dtos.Users;
+using HospitalAppointmentProject.Model.Entities;
+
+namespace HospitalAppointmentProject.Service.Mappers.Profiles;
+
+public sealed class AutoMapperConfig : Profile
+{
+    public AutoMapperConfig()
+    {
+        CreateMap<AppointmentAddRequestDto, Appointment>();
+        CreateMap<AppointmentUpdateRequestDto, Appointment>();
+        CreateMap<Appointment, AppointmentResponseDto>();
+
+        CreateMap<DoctorAddRequestDto, Doctor>();
+        CreateMap<DoctorUpdateRequestDto, Doctor>();
+        CreateMap<Doctor, DoctorResponseDto>();
+
+        CreateMap<HospitalAddRequestDto, Hospital>();
+        CreateMap<HospitalUpdateRequestDto, Hospital>();
+        CreateMap<Hospital, HospitalResponseDto>();
+
+        CreateMap<PatientAddRequestDto, Patient>();
+        CreateMap<PatientUpdateRequestDto, Patient>();
+        CreateMap<Patient, PatientResponseDto>();
+
+        CreateMap<RoleAddRequestDto, Role>();
+        CreateMap<RoleUpdateRequestDto, Role>();
+        CreateMap<Role, RoleResponseDto>();
+
+        CreateMap<UserRoleAddRequestDto, UserRole>();
+        CreateMap<UserRoleUpdateRequestDto, UserRole>();
+        CreateMap<UserRole, UserRoleResponseDto>();
+
+        CreateMap<UserAddRequestDto, User>();
+        CreateMap<UserUpdateRequestDto, User>();
+        CreateMap<User, UserResponseDto>();
+
+
+    }
+}
